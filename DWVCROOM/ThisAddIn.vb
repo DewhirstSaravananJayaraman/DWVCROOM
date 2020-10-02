7ecc30bb99200690caa14b65e3e2ffb5e388f77b
Saravanan Veerayah Jayaraman <saravanan.jayaraman@hotmail.com>
2020-10-02 08:44:03 +0800

    Replicaiting

--- DWVCROOM/ThisAddIn.vb
+++ DWVCROOM/ThisAddIn.vb
@@ -1,10 +1,10 @@
 ﻿Public Class ThisAddIn
 
-    Private Sub ThisAddIn_Startup() Handles Me.Startup
+    Private Sub ThisAddIn_Startup(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Startup
 
     End Sub
 
-    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+    Private Sub ThisAddIn_Shutdown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shutdown
 
     End Sub
 
